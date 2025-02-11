9f3c9b94f0dd4a1078c1735c44f37f265e6ce458
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-02-11 23:49:21 +0530

    lecture 02

---
+++ Lecture 02/script.js
@@ -0,0 +1,60 @@
+// var myname="sachin";
+// let age=21;
+// const pi=3.14;
+
+// // var myname="ram";
+// //  age=23
+// let abc;
+// let xyz=null;
+// console.log(myname,age,pi,"+- this is console");
+// let str = "Hello";
+// let num = 42;
+// let isTrue = true;
+// let notAssigned;
+// let emptyValue = null;
+// let person = { name: "Alice", age: 30 };
+// console.log(str, num, isTrue, notAssigned, emptyValue, person);
+
+//Arthmatic operators
+// let a = 11, b = 5;
+// console.log("sum=",a + b); // 15
+// console.log("sub",a - b); // 5
+// console.log("mul=",a * b); // 50
+// console.log("div",a / b); // 2
+// console.log("mod",a % b); // 0
+// console.log("inc=",++a);   // 11
+// console.log("dec",--b);   // 4
+
+//Asignment operators
+// let x = 10;
+// x += 5;  // 15
+// console.log(x);
+// x -= 3;  // 12
+// console.log(x);
+// x *= 2;  // 24
+// console.log(x);
+// x /= 4;  // 6
+// console.log(x);
+
+// com operator
+
+// console.log(10 == "10");  // true
+// console.log(10 === "10"); // false
+// console.log(10 != 5);      // true
+// console.log(10 !== "10"); // true
+// console.log(10 > 5);       // true
+// console.log(10 < 5);       // false
+// console.log(10 >= 10);     // true
+// console.log(5 <= 10);      // true
+
+// logical ope
+console.log(false && true); // false
+console.log(true || false); // true
+console.log(!false);         // false
+
+/*
+this is multiline
+comment 
+by 
+me 
+*/
