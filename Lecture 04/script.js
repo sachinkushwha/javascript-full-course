1e2273b613d41fa31787988d4c51e91f693e94ee
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-04-03 21:55:48 +0530

    update

---
+++ Lecture 04/script.js
@@ -0,0 +1,54 @@
+// let day=3;
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("thursday");
+//         break;
+//     case 5:
+//         console.log("friday");
+//         break;
+//     case 6:
+//         console.log("saturday");
+//         break;
+//     case 7:
+//         console.log("sunday");
+//         break;
+//     default:
+//         console('invalid input');
+
+// }
+
+// let fruit='banana';
+// switch(fruit){
+//     case 'apple':
+//         console.log('apple is red');
+       
+//     case 'banana':
+//         console.log('banana is yellow');
+       
+//     case 'graps':
+//         console.log('graps is purple');
+        
+//     default:
+//         console.log('invalid input...');
+// }
+
+let num=0;
+switch(true){
+    case (num>0):
+        console.log('positive');
+        break;
+    case (num<0):
+        console.log('neg');
+        break;
+    default:
+        console.log('zero');
+}
