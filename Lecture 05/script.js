855d4372da522e1e16e8a7aa3ce72b9719c2dae1
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-04-13 15:49:28 +0530

    update

---
+++ Lecture 05/script.js
@@ -0,0 +1,12 @@
+// if([]){
+//     console.log('ture vlaue');
+// }else{
+//     console.log('false');
+// }
+
+// console.log(Boolean({}))
+let s=" ";
+console.log(Boolean());
+if(!s){
+    console.log('enter your name');
+}
