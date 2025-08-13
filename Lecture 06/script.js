f4a0d36a096faa744a4ada2b97f97afb9f4b779d
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-08-14 01:10:50 +0530

    update

---
+++ Lecture 06/script.js
@@ -0,0 +1,25 @@
+// for(let i=1;i<=5;i++){
+//     console.log("i= "+i);
+// }
+
+// for(let i=5;i>=1;i--){
+//     console.log(i)
+// }
+
+// for(let i=1;i<=3;i++){
+//     for(let j=1;j<=2;j++){
+//         console.log(j);
+//     }
+// }
+
+// for(let i=1;i<=5;i++){
+//     if(i===3)
+//         break;
+//     console.log(i);
+// }
+
+for(let i=1;i<=5;i++){
+    if(i===3)
+        continue;
+    console.log(i)
+}
